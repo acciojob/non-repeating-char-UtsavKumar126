4c6e55ed6d0e4519f04cf75418acd868ea42cf12
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-07-19 12:26:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+function Main() {
+	let str = prompt("Enter a string"); 
+	alert(findNon(str));
+}
+function findNon(str) {
+	let obj={};
+	for(let i=0;i<str.length;i++){
+		if(obj[str[i]]>1){
+			obj[str[i]]++;
+		}
+		else{
+			obj[str[i]]=1;
+		}
+	}
+
+	for(let i=0;i<str.length;i++){
+		if(obj[str[i]]===1){
+			return str[i];
+		}
+	}
+	
+}
+Main();
+
